6cb1b68c683913ad39799221cbd14c4a35c538f3
apip2pipp <afifkhosyidzaki@gmail.com>
2025-03-01 11:38:26 +0700

    user controller pract 6 josbheet 3

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+
+class UserController extends Controller
+{
+    public function index(){
+    $user = UserModel::all();
+    return view('user',['data'=> $user]);
+}
+}
