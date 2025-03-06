0cbc2bc02e67b23a9bd348e3ed2b72df00450fc0
apip2pipp <afifkhosyidzaki@gmail.com>
2025-03-06 08:39:53 +0700

    practicum 1.1 jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,19 +11,19 @@ class UserController extends Controller
     public function index()       
     {
         // // tambah data user dengan Eluquenr Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_empat',
-            'name' => 'Manager 3',
-            'password' => Hash::make('12345')
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_TIGA',
+        //     'name' => 'Manager 3',
+        //     'password' => Hash::make('12345')
             
-            // 'name' => 'Pelanggan Pertama'
-        ];
-        UserModel::create($data);
+        //     // 'name' => 'Pelanggan Pertama'
+        // ];
+        // UserModel::create($data);
         // UserModel::insert($data); // tambahkan data ke table m_user
         // UserModel::where('username', 'customer-1') -> update($data); // update data user
         //  coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        $user = UserModel::find(1); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }
 }
