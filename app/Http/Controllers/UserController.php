c5afb57c4f5947b75945c844064986cfb4c5513a
apip2pipp <afifkhosyidzaki@gmail.com>
2025-03-06 11:56:35 +0700

    PRACTICUM 2.1 JOBSHEET 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,9 +23,33 @@ class UserController extends Controller
         // UserModel::insert($data); // tambahkan data ke table m_user
         // UserModel::where('username', 'customer-1') -> update($data); // update data user
         //  coba akses model UserModel
-        // $user = UserModel::find(1); // ambil semua data dari tabel m_user
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
         
-        $user = UserModel::where('level_id', 1)->first();
-        return view('user', ['data' => $user]);
+
+        //PRAKTIKUM 2.1 JOBSHEET 4 STEP 1
+        // $user = UserModel::find(1);
+        // return view('user', ['data' => $user]);
+
+        //PRAKTIKUM 2.1 JOBSHEET 4 STEP 4
+        // $user = UserModel::where('level_id',1)->first();
+        // return view('user', ['data' => $user]);
+
+        //PRAKTIKUM 2.1 JOBSHEET 4 STEP 6
+        // $user = UserModel::firstWhere('level_id',2);
+        // return view ('user', ['data' => $user]);
+
+        //PRAKTIKUM 2.1 JOBSHEET 4 STEP 9
+        // $user = UserModel::findOr(1,['username','name'],function(){
+        //     abort(404);
+        // });
+
+        // return view ('user',['data'=>$user]);
+        
+        //PRAKTIKUM 2.1 JOBSHEET 4 STEP 11
+        $user = UserModel::findOr(2,['username','name'],function(){
+            abort(404);
+        });
+
+        return view ('user',['data'=>$user]);
     }
 }
