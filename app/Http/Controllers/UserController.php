b9c5acf89baec3985a4b24e2d473d6bd9addd125
apip2pipp <afifkhosyidzaki@gmail.com>
2025-03-06 08:50:20 +0700

    practicum 2.1 jobsheet 4 step 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,7 +23,9 @@ class UserController extends Controller
         // UserModel::insert($data); // tambahkan data ke table m_user
         // UserModel::where('username', 'customer-1') -> update($data); // update data user
         //  coba akses model UserModel
-        $user = UserModel::find(1); // ambil semua data dari tabel m_user
+        // $user = UserModel::find(1); // ambil semua data dari tabel m_user
+        
+        $user = UserModel::where('level_id', 1)->first();
         return view('user', ['data' => $user]);
     }
 }
