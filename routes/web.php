b6224f0f709251cea79a219a7d3ba2a66fc7f38f
apip2pipp <afifkhosyidzaki@gmail.com>
2025-03-07 20:27:31 +0700

    new route for jobsheet 4 practicum 2.6

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,14 @@ Route::get('/level',[LevelController::class,'index']);
 Route::get('/kategori',[KategoriController::class,'index']);
 Route::get('/user',[UserController::class,'index']);
 
+
+//practicum 2.6 jobsheet 4
+Route::get('/user/tambah',[UserController::class,'tambah'])->name('/user/tambah');
+Route::get('/user/ubah/{id}',[UserController::class,'ubah'])->name('/user/ubah');
+Route::get('/user/hapus/{id}',[UserController::class,'hapus'])->name('/user/hapus');
+Route::get('/user',[UserController::class,'index'])->name('/user');
+Route::post('/user/tambah_simpan',[UserController::class,'tambah_simpan'])->name('/user/tambah_simpan');
+
+
 // Route :: get ('/public/user', [UserController::class, 'index' ]);
 // Route :: get ('/user', [UserController::class, 'index' ]);
