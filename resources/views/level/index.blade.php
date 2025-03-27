f301aa7f77cae96ef5066fdc0527f8d9a0f437c6
apip2pipp <afifkhosyidzaki@gmail.com>
2025-03-27 20:37:25 +0700

    .modify url for level/create

--- resources/views/level/index.blade.php
+++ resources/views/level/index.blade.php
@@ -5,113 +5,112 @@
         <div class="card-header">
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
-                <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
-                <button onclick="modalAction('{{ url('/user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+                <a class="btn btn-sm btn-primary mt-1" href="{{ url('level/create') }}">Tambah</a>
+                {{-- <button onclick="modalAction('{{ route('level.create-ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button> --}}
             </div>
         </div>
-        {{-- BENER GA NYA GATAU --}}
-        <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div> 
+
         <div class="card-body">
-            
             @if (session('success'))
-                <div class="alert alert-success alert-dismissible">
-                    <h5><i class="icon fas fa-check"></i> Berhasil</h5>
-                    {{ session('success') }}
-                </div>
+                <div class="alert alert-success">{{ session('success') }}</div>
             @endif
+
             @if (session('error'))
-                <div class="alert alert-danger alert-dismissible">
-                    <h5><i class="icon fas fa-ban"></i> Kesalahan</h5>
-                    {{ session('error') }}
-                </div>
+                <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
+
             <div class="row">
-                <div class="col-12">
+                <div class="col-md-12">
                     <div class="form-group row">
-                        <label class="col-1 control-label col-form-label">Level</label>
+                        <label class="col-1 control-label col-form-label">Filter:</label>
                         <div class="col-3">
-                            <select class="form-control" id="level_id" name="level_id" required>
+                            <select name="level_id" id="level_id" class="form-control">
                                 <option value="">- Semua -</option>
                                 @foreach ($level as $item)
-                                    <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                                    <option value="{{ $item->level_id }}">{{ $item->level_name }}</option>
                                 @endforeach
                             </select>
                             <small class="form-text text-muted">Level Pengguna</small>
                         </div>
                     </div>
-                    <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Username</th>
-                                <th>Nama</th>
-                                <th>Level Pengguna</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                    </table>
                 </div>
             </div>
-        @endsection
 
-        @push('css')
-        @endpush
+            <table id="table-level" class="table table-bordered table-striped table-hover table-sm">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Kode Level</th>
+                        <th>Nama</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
 
-        @push('js')
-            <script>
-                function modalAction(url = ''){ 
-    $('#myModal').load(url,function(){ 
-        $('#myModal').modal('show'); 
-    }); 
-}
-                var dataUser;
-                $(document).ready(function() {
-                    var dataUser = $('#table_user').DataTable({
-                        serverSide: true,
-                        ajax: {
-                            url: "{{ url('user/list') }}",
-                            dataType: "json",
-                            type: "POST",
-                            data: function(d) {
-                                d.level_id = $('#level_id').val();
-                            }
-                        },
-                        columns: [{
-                                data: "DT_RowIndex",
-                                className: "text-center",
-                                orderable: false,
-                                searchable: false
-                            },
-                            {
-                                data: "username",
-                                className: "",
-                                orderable: true,
-                                searchable: true
-                            },
-                            {
-                                data: "nama",
-                                className: "",
-                                orderable: true,
-                                searchable: true
-                            },
-                            {
-                                data: "level.level_nama",
-                                className: "",
-                                orderable: false,
-                                searchable: false
-                            },
-                            {
-                                data: "aksi",
-                                className: "",
-                                orderable: false,
-                                searchable: false
-                            }
-                        ]
-                    });
+    <div 
+        id="myModal" 
+        class="modal fade animate shake" 
+        tabindex="-1" role="dialog" 
+        data-backdrop="static" 
+        data-keyboard="false" 
+        data-width="75%" 
+        aria-hidden="true">
+    </div>
+@endsection
 
-                    $('#level_id').change(function() {
-                        dataUser.ajax.reload();
-                    });
-                });
-            </script>
-        @endpush
+@push('js')
+<script>
+    function modalAction(url) {
+        $('#myModal').load(url, function() {
+            $('#myModal').modal('show');
+        });
+    }
+
+    let dataLevel;
+    $(document).ready(() => {
+        dataLevel = $('#table-level').DataTable({
+            serverSide: true,
+            ajax: {
+                "url": "{{ route('level.list') }}",
+                "dataType": "json",
+                "type": "POST",
+                "data": (d) => {
+                    d.level_id = $('#level_id').val()
+                }
+            },
+            columns: [
+                {
+                    data: "DT_RowIndex",
+                    className: "text-center",
+                    orderable: false,
+                    searchable: false
+                },
+                {
+                    data: "level_kode",
+                    className: "",
+                    orderable: true,
+                    searchable: true
+                },
+                {
+                    data: "level_name",
+                    className: "",
+                    orderable: true,
+                    searchable: true
+                },
+                {
+                    data: "aksi",
+                    className: "",
+                    orderable: false,
+                    searchable: false
+                }
+            ]
+        });
+
+        $('#level_id').on('change', () => {
+            dataLevel.ajax.reload();
+        });
+    });
+</script>
+@endpush
