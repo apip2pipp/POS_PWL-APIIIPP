1328c193c59c4ad20d1fcf1474db33a70d854a1a
apip2pipp <afifkhosyidzaki@gmail.com>
2025-03-27 20:36:48 +0700

    .add create for level

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -25,4 +25,42 @@ class LevelController extends Controller
         ]);
     }
 
+    public function list(Request $req) 
+    {
+        $level = LevelModel::select( 'level_id', 'level_kode', 'level_name');
+
+        if ($req->level_id) {
+            $level->where('level_id', $req->level_id);
+        }
+
+        return DataTables::of($level)
+            ->addIndexColumn()
+            ->addColumn('aksi', function ($level) {
+                $detailUrl = route('level.show', ['id' => $level->level_id]);
+                $editUrl = route('level.edit-ajax', ['id' => $level->level_id]);
+                $deleteAjax = route('level.delete-ajax', ['id' => $level->level_id]);
+                
+                return <<<HTML
+                <button onclick="modalAction('{$detailUrl}')" class="btn btn-info btn-sm">Detail</button>
+                <button onclick="modalAction('{$editUrl}')" class="btn btn-warning btn-sm">Edit</button>
+                <button onclick="modalAction('{$deleteAjax}')" class="btn btn-danger btn-sm">Hapus</button>
+                HTML;
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
+    }
+
+    public function create()
+    {
+        return view('level.create', [
+            'breadcrumb' => (object) [
+                'title' => 'Tambah Level Pengguna',
+                'list' => ['Home', 'Level', 'Tambah']
+            ],
+            'page' => (object) [
+                'title' => 'Tambah level pengguna baru'
+            ],
+            'activeMenu' => 'level'
+        ]);
+    }
 }
