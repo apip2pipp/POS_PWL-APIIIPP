6e8eae8896f438dbfd612a7d7593ec04b2374f58
apip2pipp <afifkhosyidzaki@gmail.com>
2025-03-31 07:12:38 +0700

    .add index for barang (not fixed)

---
+++ resources/views/barang/index.blade.php
@@ -0,0 +1,130 @@
+@extends('layouts.template')
+
+@section('content')
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{ $page->title }}</h3>
+            <div class="card-tools">
+                <a href="{{ route('barang.create') }}" class="btn btn-sm btn-primary mt-1">Tambah</a>
+                <button onclick="modalAction('{{ route('barang.create-ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+            </div>
+        </div>
+
+        <div class="card-body">
+            @if (session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+
+            @if (session('error'))
+                <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Filter:</label>
+                        <div class="col-3">
+                            <select name="barang_nama" id="barang_nama" class="form-control">
+                                <option value="">- Semua -</option>
+                                @foreach ($barang as $item)
+                                    <option value="{{ $item->kategori_id }}">{{ $item->kategori_nama }}</option>
+                                @endforeach
+                            </select>
+                            <small class="form-text text-muted">Data Barang</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <table id="table-barang" class="table table-bordered table-striped table-hover table-sm">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Kode Barang</th>
+                        <th>Nama Barang</th>
+                        <th>Harga Beli</th>
+                        <th>Harga Jual</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+
+    <div 
+        id="myModal" 
+        class="modal fade animate shake" 
+        tabindex="-1" role="dialog" 
+        data-backdrop="static" 
+        data-keyboard="false" 
+        data-width="75%" 
+        aria-hidden="true">
+    </div>
+@endsection
+
+@push('js')
+<script>
+    function modalAction(url) {
+        $('#myModal').load(url, function() {
+            $('#myModal').modal('show');
+        });
+    }
+
+    let dataBarang;
+    $(document).ready(() => {
+        dataBarang = $('#table-barang').DataTable({
+            serverSide: true,
+            ajax: {
+                "url": "{{ route('barang.list') }}",
+                "dataType": "json",
+                "type": "POST",
+                "data": (d) => {
+                    d.barang_id = $('#barang_id').val()
+                }
+            },
+            columns: [
+                {
+                    data: "DT_RowIndex",
+                    className: "text-center",
+                    orderable: false,
+                    searchable: false
+                },
+                {
+                    data: "barang_kode",
+                    className: "",
+                    orderable: true,
+                    searchable: true
+                },
+                {
+                    data: "barang_nama",
+                    className: "",
+                    orderable: true,
+                    searchable: true
+                },
+                {
+                    data: "harga_beli",
+                    className: "",
+                    orderable: true,
+                    searchable: true
+                },
+                {
+                    data: "harga_jual",
+                    className: "",
+                    orderable: true,
+                    searchable: true
+                },
+                {
+                    data: "aksi",
+                    className: "",
+                    orderable: false,
+                    searchable: false
+                }
+            ]
+        });
+
+        $('#barang_id').on('change', () => {
+            dataBarang.ajax.reload();
+        });
+    });
+</script>
+@endpush
