51c2de028167b2b476687e4f6d59c8cae946ac6d
apip2pipp <afifkhosyidzaki@gmail.com>
2025-05-07 20:15:21 +0700

    .add import view

--- resources/views/barang/import-excel.blade.php
+++ resources/views/barang/import-excel.blade.php
@@ -1,74 +1,76 @@
-<form action="{{ url('barang.import.excel') }}" method="POST" id="form-import" enctype="multipart/form-data">
+<form action="{{ route('barang.import.excel.post') }}" method="post" id="form-import" enctype="multipart/form-data">
     @csrf
+
     <div id="modal-master" class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Import Data Barang</h5>
-                <button type="button" class="close" data-dismiss="modal" aria- label="Close"><span aria-hidden="true">&times;</span></button>
+                <h5 class="modal-title" id="modalLabel">Import Data Barang</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
+
             <div class="modal-body">
                 <div class="form-group">
                     <label>Download Template</label>
-                    <a href="{{ asset('template_barang.xlsx') }}" class="btn btn-info btn- sm" download><i
-                            class="fa fa-file-excel"></i>Download</a>
-                    <small id="error-kategori_id" class="error-text form-text text-
-    danger"></small>
+                    <a href="{{ asset('template_barang.xlsx') }}" class="btn btn-info btn-sm">
+                        <i class="fa fa-file-excel"></i>
+                        Download
+                    </a>
+                    <small id="error-kategori-id" class="error-text form-text text-danger"></small>
                 </div>
+
                 <div class="form-group">
                     <label>Pilih File</label>
-                    <input type="file" name="file_barang" id="file_barang" class="form-
-    control" required>
-                    <small id="error-file_barang" class="error-text form-text text-
-    danger"></small>
+                    <input type="file" name="file_barng" id="file_barang" class="form-control" required>
+                    <small id="error-file-barang" class="error-text form-text text-danger"></small>
                 </div>
             </div>
+
             <div class="modal-footer">
-                <button type="button" data-dismiss="modal" class="btn btn- warning">Batal</button>
+                <button type="button" class="btn btn-warning" data-dismiss="modal">Batal</button>
                 <button type="submit" class="btn btn-primary">Upload</button>
             </div>
         </div>
     </div>
 </form>
+
 <script>
     $(document).ready(function() {
-        $("#form-import").validate({
+        $('#form-import').validate({
             rules: {
                 file_barang: {
                     required: true,
                     extension: "xlsx"
-                },
+                }
             },
             submitHandler: function(form) {
-                var formData = new FormData(form); // Jadikan form ke FormData untuk
-                menghandle file
                 $.ajax({
                     url: form.action,
                     type: form.method,
-                    data: formData, // Data yang dikirim berupa FormData processData: false, // setting processData dan contentType ke false,
-                    untuk menghandle file
+                    data: new FormData(form),
                     contentType: false,
+                    processData: false,
                     success: function(response) {
-                        if (response.status) { // jika sukses
+                        if (response.status) {
                             $('#myModal').modal('hide');
                             Swal.fire({
                                 icon: 'success',
                                 title: 'Berhasil',
-                                text: response.message
+                                text: response.message,
                             });
-                            tableBarang.ajax.reload(); // reload datatable
-                        } else { // jika error
+                        } else {
                             $('.error-text').text('');
                             $.each(response.msgField, function(prefix, val) {
                                 $('#error-' + prefix).text(val[0]);
                             });
-                            Swal.fire({
-                                icon: 'error',
-                                title: 'Terjadi Kesalahan',
-                                text: response.message
-                            });
                         }
+                    },
+                    error: function(xhr, status, error) {
+                        alert('An error occurred while importing the file.');
                     }
                 });
+
                 return false;
             },
             errorElement: 'span',
@@ -76,12 +78,12 @@
                 error.addClass('invalid-feedback');
                 element.closest('.form-group').append(error);
             },
-            highlight: function(element, errorClass, validClass) {
+            highlight: function (element, errorClass, validClass) {
                 $(element).addClass('is-invalid');
             },
-            unhighlight: function(element, errorClass, validClass) {
+            unhighlight: function (element, errorClass, validClass) {
                 $(element).removeClass('is-invalid');
             }
-        });
+        })
     });
 </script>
