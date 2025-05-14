c47c72453eed1b32add612a8dfc117162e4af0e9
apip2pipp <afifkhosyidzaki@gmail.com>
2025-05-14 08:48:38 +0700

    .add logout api

--- app/Http/Controllers/Api/LogoutController.php
+++ app/Http/Controllers/Api/LogoutController.php
@@ -5,16 +5,19 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Tymon\JWTAuth\Facades\JWTAuth;
 class LogoutController extends Controller
 {
     public function __invoke(Request $request)
     {
-        // Hapus token akses saat ini
-        $request->user()->currentAccessToken()->delete();
 
-        return response()->json([
-            'message' => 'Logged Out Succes'
-        ]);
+        $removeToken = JWTAuth::invalidate(JWTAuth::getToken());
+
+        if ($removeToken) {
+            return response()->json([
+                'succes' => true,
+                'message' => 'Logout Succesfull',
+            ],200);
+        }
     }
 }
