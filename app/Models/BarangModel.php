9e80105513e7aee186ba305d64ea4fb5dff668d9
apip2pipp <afifkhosyidzaki@gmail.com>
2025-05-15 22:10:23 +0700

    `Added 'add_image' field to BarangModel and created migration to add 'add_image' column to m_user table.`

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -18,7 +18,8 @@ class BarangModel extends Model
         'barang_kode',
         'barang_nama',
         'harga_beli',
-        'harga_jual'
+        'harga_jual',
+        'add_image'
     ];
 
     public function kategori(): BelongsTo
