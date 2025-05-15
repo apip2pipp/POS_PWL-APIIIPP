7fa92a6571bb98db62178311838f21215211949c
apip2pipp <afifkhosyidzaki@gmail.com>
2025-05-15 22:10:09 +0700

    .add image Controller

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -16,17 +16,23 @@ class RegisterController extends Controller
             'nama' => 'required',
             'password' => 'required|min:5|confirmed',
             'level_id' => 'required',
+            'add_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
         ]);
 
     if ($validator->fails()) {
         return response()->json($validator->errors(),422);
     }
+    // simpan gambar ke folder public/storage/imagesu
+    $req->add_image->store('images', 'public');
 
     $user = UserModel::create([
+
+
         'username' => $req->username,
         'nama' => $req->nama,
         'password' => bcrypt($req->password),
         'level_id' => $req->level_id,
+        'add_image' => $req->add_image->hashName(),
     ]);
 
     //return response JSON user is created
